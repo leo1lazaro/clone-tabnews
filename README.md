8fd93b68d388c20038a465a3d1fc9ec4cfed5740
Leonardo Lazaro <144863110+leo1lazaro@users.noreply.github.com>
2025-06-25 16:03:42 +0000

    Adiciona scripts `lint:check ` e `lint:fix `

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para desenvolvimento de ponta a ponta
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Outro deploy</h1>
+function Home() {
+  return <h1>Outro deploy</h1>;
 }
 
 export default Home;
